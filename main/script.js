6858011febaa3874d5c8ef11626809e469fb516a
Dhruvesh Singh Om <dhruveshsinghom@Dhruveshs-MacBook-Air.local>
2025-02-07 19:33:42 +0530

    project

---
+++ main/script.js
@@ -0,0 +1,27 @@
+const button = document.getElementById('button');
+
+    function moveButtonAway(event) {
+      const buttonRect = button.getBoundingClientRect();
+      const buttonX = buttonRect.left + buttonRect.width / 2;
+      const buttonY = buttonRect.top + buttonRect.height / 2;
+
+      const distX = event.clientX - buttonX;
+      const distY = event.clientY - buttonY;
+      const distance = Math.sqrt(distX * distX + distY * distY);
+
+      if (distance < 100) {
+        const moveX = (distX / distance) * 50; 
+        const moveY = (distY / distance) * 50;
+
+        button.style.left = button.offsetLeft - moveX + 'px';
+        button.style.top = button.offsetTop - moveY + 'px';
+      }
+    }
+
+    document.addEventListener('mousemove', moveButtonAway);
+
+document.getElementById('second').addEventListener('click',function(){
+    alert("❤️CONGRATULATIONS ❤️",true);
+})
+
+
